c7b9b129896df42160a3569e52042a8dbbcfec74
Mohammad Abdelrahman <albossmohammad83@gmail.com>
2021-01-12 13:26:07 -0800

    Add files via upload

---
+++ src/MyFirstLab.java
@@ -0,0 +1,33 @@
+/*
+ *  Assignment Chapter 1 Lab
+ *  Description: printing on console
+ *  Name: Mohammad Abdelrahman
+ *  ID: 920158652
+ *  Class: CSC 211-02
+ *  Semester: Fall 2020
+ */
+public class MyFirstLab {
+    public static void main(String[] args) {
+
+        // Start the program by greeting the user
+        System.out.println("Hello World!");
+        System.out.println("My name is Mohammad.");
+
+        // State that I'm an SFSU student on a single line.
+        System.out.print("I am a ");
+        System.out.print("student at ");
+        System.out.println("SFSU.");
+
+        // This print statement prints an empty line.
+        System.out.println();
+
+        /*
+            This is another set of print statements
+            that states that I'm an SFSU student,
+            but it prints it with multiple line breaks.
+         */
+        System.out.println("I am a ");
+        System.out.println("student at ");
+        System.out.println("SFSU.");
+    }
+}
